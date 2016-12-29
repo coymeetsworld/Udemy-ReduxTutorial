6991c78c4966b157b2ad4948ca23912f8f833313
Coy Sanders <coymeetsworld@gmail.com>
2016-12-29 00:30:38 -0800

    Add support for CHANGE_NAME action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,15 +2,29 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-//pure function in redux: reducer
-
+// Pure function in redux: reducer
+// Takes existing state and returns a new one based on performing the action.
 var reducer = (state = {name: 'Anonymous'}, action) => {
-	//state = state || {name: 'Anonymous'};
-	
-	return state;	
+
+	switch(action.type) {
+		case 'CHANGE_NAME':
+			return {
+				...state,
+				name: action.name
+			};
+		default:
+			return state;
+	}	
 };
 
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+store.dispatch({
+	type: 'CHANGE_NAME',
+	name: 'Coy'	
+});
+
+console.log('currentState', store.getState());
